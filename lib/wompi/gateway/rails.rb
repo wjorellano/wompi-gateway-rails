0d350da21bcf072cdeafa27070ec82132769f66f
wilman orellano <wilmanjunior2001@gmail.com>
2023-10-11 09:44:29 -0500

    Fisrt commit

---
+++ lib/wompi/gateway/rails.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "rails/version"
+
+module Wompi
+  module Gateway
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
